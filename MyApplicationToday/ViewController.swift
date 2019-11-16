35db94cb9950ef06503671b9076db35cd0ef22af
Dmitriy <da.savushkin4@gmail.com>
2019-11-16 16:15:21 +0300

    Initial Commit

---
+++ MyApplicationToday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApplicationToday
+//
+//  Created by Дмитрий on 16/11/2019.
+//  Copyright © 2019 Дмитрий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
